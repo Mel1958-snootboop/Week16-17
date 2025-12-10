2807cc83092d6594466ff17c465a9c000f40dab1
Melanie <1958Melanie.Castillo@gmail.com>
2025-12-09 22:40:45 -0800

    commit

--- photo-gallery/src/pages/Tab1.tsx
+++ photo-gallery/src/pages/Tab1.tsx
@@ -1,22 +1,47 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonListHeader, IonItem, IonLabel } from '@ionic/react';
+import React, {useState, useEffect} from 'react';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
+  // dataset state variable to hold JSON data from WP
+  // to meet Ionic's requirement for typescript data types
+  // set type to <any[]> on next line
+  const [dataset, setDataset] = useState<any[]>([]);
+  // URL for WP JSON REST endpoint
+  const dataURL = "https://dev-srjc-fall-2025.pantheonsite.io/wp-json/twentytwentyone-child/v1/special";
+  // useEffect() to get JSON data and populate dataset state variable
+  useEffect(() => {
+    fetch(dataURL) // fetch() to load raw json string
+    .then(response => response.json()) // json() to convert raw string to json object
+    .then(data => setDataset(data)) // react state set function to populate data state var
+  },[])
+  
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Stuffs</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">More Stuffs</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        {/* use map() to loop through JSON array returned from WP */}
+        <IonList id="stuff-list">
+          <IonListHeader>Stuffs</IonListHeader>
+          {dataset.map((item, index) => (
+            <IonItem lines="inset" key={index}>
+              <IonLabel>
+                <h4>{item.post_title}</h4>
+                <p>{item.thing_description}</p>
+                <address>{item.thing_address}</address>
+              </IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
